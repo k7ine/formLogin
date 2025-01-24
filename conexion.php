d18319b5452ad7579830c2be39c78c5be79ab314
k7ine <pablomengod02@gmail.com>
2025-01-24 10:50:19 +0100

    Database connection

--- conexion.php
+++ conexion.php
@@ -1,19 +1,15 @@
 <?php
-
-// Establece la conexión
-$dsn = 'mysql:dbname=usuariodb;host=localhost:3306'; //nombre Base datos y dónde está la BD
+// Establish the connection
+$dsn = 'mysql:dbname=usuariodb;host=localhost:3306'; // Name and data base location
 $usuario = 'test';
 $contrasena = 'test';
 
 try {
-
 $conexion = new PDO($dsn, $usuario, $contrasena);
 $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo "Conexión Establecida con la BD en Docker";
-
 }
-catch (PDOException $e) { //en caso de detectar un error lo muestra
+catch (PDOException $e) { // Error case
 echo 'Falló la conexión: ' . $e->getMessage();
 }
-
 ?>
