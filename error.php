d05758695bafc226853b1d5d121d8162396aad3d
k7ine <pablomengod02@gmail.com>
2025-01-25 16:49:50 +0100

    Connection error

---
+++ error.php
@@ -0,0 +1,15 @@
+<?php
+include ("conexion.php");
+
+// Si el usuario no existe o la contraseña está mal el usuario accederá a esta página
+
+echo "<br>";
+
+echo "El usuario no existe o la contraseña es incorrecta, intentelo de nuevo o registrese.";
+
+echo "<br>";
+
+echo "Registrate en: <a href='./registro.php'>Registro</a>";
+
+
+?>
